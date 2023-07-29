f8f58520ccc08f3ab777abe552de1cb057de7f9e
shahbaz ramzan <shahbaz.ramzan@codedistrict.com>
2023-07-29 21:17:30 +0500

    july -29

--- july-28.js
+++ july-28.js
@@ -1,35 +1,38 @@
-console.log("hello");
-Write a function that returns the number of properties in an object.
-Input: {name: 'John', age: 25, city: 'New York'}
-Output: 3
+// console.log("hello");
+// Write a function that returns the number of properties in an object.
+// Input: {name: 'John', age: 25, city: 'New York'}
+// Output: 3
 
-let obj = { name: "John", age: 25, city: "New York", country: "pakistan" };
+// let obj = { name: "John", age: 25, city: "New York", country: "pakistan" };
 
-let result = Object.keys(obj).length;
-console.log(result);
+// let result = Object.keys(obj).length;
+// console.log(result);
 
-console.log(Object.keys(obj).length);
-console.log(Object.keys(obj).length);
+// console.log(Object.keys(obj).length);
+// console.log(Object.keys(obj).length);
 
-Write a function that checks if an object is empty.
-Input: {}
-Output: true
+// Write a function that checks if an object is empty.
+// Input: {}
+// Output: true
 
-function checksObject() {
-  return Object.keys(Input).length === 0;
-}
-let Input = {};
+// function checksObject() {
+//   return Object.keys(Input).length === 0;
+// }
+// let Input = {};
 
-console.log(checksObject(Input));
+// console.log(checksObject(Input));
 
-let obj = {};
+// let obj = {};
 
-function checksEmpty() {
-  return Object.keys(obj).length === 0;
-}
-console.log(checksEmpty(obj));
+// function checksEmpty() {
+//   return Object.keys(obj).length === 0;
+// }
+// console.log(checksEmpty(obj));
 
-Write a function that checks if a given key exists in an object.
-Input: {name: 'John', age: 25, city: 'New York'}, 'age'
-Output: true
+// Write a function that checks if a given key exists in an object.
+// Input: {name: 'John', age: 25, city: 'New York'}, 'age'
+// Output: true
 
+// Write a function that checks if a given key exists in an object.
+// Input: {name: 'John', age: 25, city: 'New York'}, 'age'
+// Output: true
---
+++ july-29.js
@@ -0,0 +1,25 @@
+// Splice: Write a function that takes an array of strings and removes the first two elements.
+// Example Input: ['a', 'b', 'c']
+// Expected Output: ['c']
+
+// function fun(arr) {
+//   return arr.splice(0, 2);
+// }
+
+// let arr = ["a", "b", "c"];
+// console.log(fun(arr)); // removed item
+// console.log(arr); // remaining item
+
+// Every: Write a function that takes an array of numbers and returns true if all the numbers are positive.
+// Example Input: [1, 2, 3]
+// Expected Output: true
+
+let arr = [1, 2, 3];
+
+function fun(arr) {
+  return arr.every((el) => {
+    return el > 0;
+  });
+}
+
+console.log(fun(arr));
